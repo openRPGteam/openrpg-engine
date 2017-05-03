887c7a4d5423208426e2b0f4d6adadb65d95e7dc
Danny <liquidcore7@gmail.com>
2017-05-03 17:32:35 +0300

    moved resources to subfolder

--- render.py
+++ render.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os, random
 
-WORLD_MAP = "terrain2.jpg"
-SPRITES_DIR = "sprites/"
+WORLD_MAP = "test_resources/terrain.jpg"
+SPRITES_DIR = "test_resources/sprites/"
 
 
 def get_background(char_pos, size=300):
