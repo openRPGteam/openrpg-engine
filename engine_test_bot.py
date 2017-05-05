67edeb199f7f3d7a9f6d8fb56cff77c5313b1bf9
Danny <liquidcore7@gmail.com>
2017-05-05 22:01:49 +0300

    added comments, removed obsolete file handle

--- engine_test_bot.py
+++ engine_test_bot.py
@@ -5,9 +5,12 @@ from random import randint
 
 bot = telebot.TeleBot("TOKEN")
 
+# leave blank
 ACTIVE_USERS = {}
+# run server in serv/ directory, enter its url here
 PHOTO_URL = "http://url.com/"
 
+# controls
 def draw_buttons():
     keyboard = telebot.types.InlineKeyboardMarkup()
     left = telebot.types.InlineKeyboardButton("<-", callback_data='/left')
@@ -24,29 +27,38 @@ def help(message):
 
 @bot.message_handler(commands=['spawn'])
 def spawn(message):
+    # spawns in a random position
     map_spawn_pos = [randint(200, 2800), randint(200, 1500)]
+    # creates class for moving. see engine.py
     usr = engine.character('hero.jpg', map_pos=map_spawn_pos)
     move = engine.mover(usr)
+    # processes and saves image in serv/ path
     fname = engine.save_background(move.spawn(), "serv/{0}{1}.jpg".format(message.chat.id, usr.getpos()))
+    # sends image url to user
     m = bot.send_message(message.chat.id, '''<a href="{0}{1}">.</a>'''.format(PHOTO_URL, fname.split("/")[1]), reply_markup=draw_buttons(), parse_mode='html')
+    # now he`s in ACTIVE USERS, his position will be stored here
     ACTIVE_USERS[message.chat.id] = [move, m.message_id]
 
 @bot.callback_query_handler(lambda query: query.data=='/left')
 def left(query):
+    # TODO: remove if statement: obsolete since buttons release
     if query.message.chat.id not in ACTIVE_USERS.keys():
         bot.send_message(query.message.chat.id, "Spawn yourself first")
     else:
+        # remove previous image
         os.remove('serv/{0}{1}.jpg'.format(query.message.chat.id,
                                                   ACTIVE_USERS[query.message.chat.id][0].user.getpos()))
+        # generate new one
         fname = engine.save_background(ACTIVE_USERS[query.message.chat.id][0].left(),
                                        "serv/{0}{1}.jpg".format(query.message.chat.id,
                                                                 ACTIVE_USERS[query.message.chat.id][0].user.getpos()))
-        fname_handle = open(fname, 'rb')
+        # edit message sent by /spawn instead of sending a new one
         bot.edit_message_text(
             '''<a href="{0}{1}">.</a>'''.format(PHOTO_URL, fname.split('/')[1]),
             chat_id=query.message.chat.id, message_id=ACTIVE_USERS[query.message.chat.id][1], parse_mode='html', reply_markup=draw_buttons())
-        fname_handle.close()
+        
 
+# further query handlers are equivalent
 @bot.callback_query_handler(lambda query: query.data=='/right')
 def right(query):
     if query.message.chat.id not in ACTIVE_USERS.keys():
@@ -57,12 +69,10 @@ def right(query):
         fname = engine.save_background(ACTIVE_USERS[query.message.chat.id][0].right(),
                                        "serv/{0}{1}.jpg".format(query.message.chat.id,
                                                            ACTIVE_USERS[query.message.chat.id][0].user.getpos()))
-        fname_handle = open(fname, 'rb')
         bot.edit_message_text(
             '''<a href="{0}{1}">.</a>'''.format(PHOTO_URL, fname.split('/')[1]),
             chat_id=query.message.chat.id, message_id=ACTIVE_USERS[query.message.chat.id][1], parse_mode='html',
             reply_markup=draw_buttons())
-        fname_handle.close()
 
 @bot.callback_query_handler(lambda query: query.data=='/up')
 def up(query):
@@ -74,12 +84,10 @@ def up(query):
         fname = engine.save_background(ACTIVE_USERS[query.message.chat.id][0].up(),
                                        "serv/{0}{1}.jpg".format(query.message.chat.id,
                                                            ACTIVE_USERS[query.message.chat.id][0].user.getpos()))
-        fname_handle = open(fname, 'rb')
         bot.edit_message_text(
             '''<a href="{0}{1}">.</a>'''.format(PHOTO_URL, fname.split('/')[1]),
             chat_id=query.message.chat.id, message_id=ACTIVE_USERS[query.message.chat.id][1], parse_mode='html',
             reply_markup=draw_buttons())
-        fname_handle.close()
 
 @bot.callback_query_handler(lambda query: query.data=='/down')
 def down(query):
@@ -91,18 +99,19 @@ def down(query):
         fname = engine.save_background(ACTIVE_USERS[query.message.chat.id][0].down(),
                                        "serv/{0}{1}.jpg".format(query.message.chat.id,
                                                            ACTIVE_USERS[query.message.chat.id][0].user.getpos()))
-        fname_handle = open(fname, 'rb')
         bot.edit_message_text(
             '''<a href="{0}{1}">.</a>'''.format(PHOTO_URL, fname.split('/')[1]),
             chat_id=query.message.chat.id, message_id=ACTIVE_USERS[query.message.chat.id][1], parse_mode='html',
             reply_markup=draw_buttons())
-        fname_handle.close()
 
 @bot.message_handler(commands=['end_game'])
 def end_game(message):
+    # remove last file
     os.remove('serv/{0}{1}.jpg'.format(message.chat.id,
                                               ACTIVE_USERS[message.chat.id][0].user.getpos()))
+    # user is not active anymore, forget his position
     del ACTIVE_USERS[message.chat.id]
     bot.send_message(message.chat.id, "Game stopped")
 
+# burn, baby, burn
 bot.polling(none_stop=True)
