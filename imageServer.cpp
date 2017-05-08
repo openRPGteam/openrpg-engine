e1289b7f9fb79f6a272dda7de8b3f72a6f95ef08
Danny <liquidcore7@gmail.com>
2017-05-08 21:07:41 +0300

    return url instead of filename

--- imageServer.cpp
+++ imageServer.cpp
@@ -38,7 +38,7 @@ std::string imageServer::runPy(const std::vector<std::string> &parsedReq,
 		positions.insert({parsedReq[0], spawned});
 		std::string map = spawned;
 		system(("python3 srv.py spawn " + map + " " + filename).c_str());
-		return filename + ".jpg";
+		return _serv_ip + filename + ".jpg";
 	} else {
 		std::string pos = positions[parsedReq[0]],
 				filename = parsedReq[0] + std::to_string(random() % 200);
@@ -48,7 +48,7 @@ std::string imageServer::runPy(const std::vector<std::string> &parsedReq,
 		// new position
 		positions[parsedReq[0]].usrPos.first += 20 * std::stoi(parsedReq[1]);
 		positions[parsedReq[0]].usrPos.second += 20 * std::stoi(parsedReq[2]);
-		return filename + ".jpg";
+		return _serv_ip + filename + ".jpg";
 	}
 }
 
@@ -79,12 +79,24 @@ imageServer::imageServer(const int port) : servInstance(new Server)
 			*response << "HTTP/1.1 200 OK\r\nContent-Length: " <<
 		              responseData.length() << "\r\n\r\n" << responseData;
 		};
+	// ends game for usrid
+	servInstance->resource["^/end/([0-9]+)$"]["GET"] =
+	[this] (std::shared_ptr<Server::Response> response,
+	        std::shared_ptr<Server::Request> request)
+	{
+		this->positions.erase(request->path_match[1]);
+		std::string responseData("user deleted");
+		*response << "HTTP/1.1 200 OK\r\nContent-Length: " <<
+	              responseData.length() << "\r\n\r\n" << responseData;
+	};
 	// TODO: remove this testing stuff
 	servInstance->resource["^/echo$"]["POST"] =
 	[] (std::shared_ptr<Server::Response> response,
 	    std::shared_ptr<Server::Request> request)
 	{
 		std::string responseData = request->content.string();
+		if (responseData.empty())
+			responseData = "Hello, World!";
 		*response << "HTTP/1.1 200 OK\r\nContent-Length: " <<
 	              responseData.length() << "\r\n\r\n" << responseData;
 	};
