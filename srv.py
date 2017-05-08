21666fc16bc554ab12c17319a69969936e7fd25d
Danny <liquidcore7@gmail.com>
2017-05-08 21:40:34 +0300

    moved saved files to serv/

--- srv.py
+++ srv.py
@@ -2,16 +2,18 @@ import engine
 import sys
 import os
 
+SAVE_DIR='serv/'
+
 if __name__ == '__main__' :
     parsed = list(map(lambda x : int(x), sys.argv[2:-1]))
     if sys.argv[1] == 'spawn' :
-        for file in os.listdir('.') :
+        for file in os.listdir('serv') :
             if file.startswith(sys.argv[-1][:-3]):
                 os.remove(file)
         mv = engine.mover(engine.character('test_resources/hero.png', [parsed[0], parsed[1]]))
-        engine.save_background(mv.spawn(), sys.argv[-1] + '.jpg')
+        engine.save_background(mv.spawn(), SAVE_DIR + sys.argv[-1] + '.jpg')
     else:
-        for file in os.listdir('.') :
+        for file in os.listdir('serv') :
             if file.startswith(sys.argv[-1][:-3]):
                 os.remove(file)
         mv = engine.mover(engine.character('test_resources/hero.png', [parsed[0], parsed[1]], [parsed[2], parsed[3]]))
@@ -24,4 +26,4 @@ if __name__ == '__main__' :
             back = mv.up()
         elif parsed[5] == 1:
             back = mv.down()
-        engine.save_background(back, sys.argv[-1] + '.jpg')
+        engine.save_background(back, SAVE_DIR + sys.argv[-1] + '.jpg')
